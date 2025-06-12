1de7313ac027f23a9b35bd4168f363909d8e5523
rafuwong <rafuyiwong@gmail.com>
2025-06-12 14:49:20 -0500

    Completed Exercise 12 and 13
    Removed ".skip" from tests in 12_calculator and 13_palindromes
    All tests pass.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+}
+
+const subtract = function(a, b) {
+  return a - b;
+};
+
+const sum = function(arr) {
+  return arr.reduce((total, num) => total + num, 0);
+};
+
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1);
+};
+
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	for (let i = a; 1 > 0; i--) {
+    if ((i === 0) || (i === 1)) {
+      return 1;
+    } else if (i > 1) {
+      return i * factorial(i - 1);
+    }
+  }
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    // Convert the input to a string and remove spaces and punctuation
+    let copiedwords = words.toString().toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reversedWord = copiedwords.split('').reverse().join('');
+    if (reversedWord === copiedwords) {
+        return true;
+    }
+ return false;
 };
 
 // Do not edit below this line
